8fb210c3e9933a0de5770af6ed43d3c1d2945e26
devatlarge <zxvf@tutanota.com>
2021-05-27 23:00:27 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [cryptoCoin, setCryptoCoin] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetchCryptoApi();
+  }, []);
+
+  const fetchCryptoApi = async () => {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+    );
+    const data = await response.json();
+    setCryptoCoin(data);
+  };
+
+  const searchQueryHandler = (e) => {
+    setSearchQuery(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const cryptoCoins = cryptoCoin.map((coin) => {
+    return (
+      <div key={coin.id}>
+        {" "}
+        <p> {coin.id} </p>
+        <div> {coin.image} </div>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-	    <header><h1>CryptoWatch</h1></header>
+      <form onSubmit={formSubmitHandler}>
+        <h3>Search Crypto</h3>
+        <input
+          onChange={searchQueryHandler}
+          value={searchQuery}
+          type="text"
+          maxLength={20}
+          placeholder="Search cryptocurrency..."
+        />
+      </form>
+      {cryptoCoins}
     </div>
   );
-}
+};
 
 export default App;
